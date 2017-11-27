84346c271511d84f870c40a886641d183f09a4e0
Charlhye <carlosbmtz@gmail.com>
2017-11-26 20:25:01 -0600

    Database updated

--- Centro-Gerontologico.sql
+++ Centro-Gerontologico.sql
@@ -79,7 +79,7 @@ CREATE TABLE `cuestionario_resuelto_respuesta` (
   PRIMARY KEY (`idCuestionarioR`,`idRespuesta`),
   KEY `idRespuesta_idx` (`idRespuesta`),
   CONSTRAINT `idCuestionarioR` FOREIGN KEY (`idCuestionarioR`) REFERENCES `cuestionario_resuelto` (`idCuestionario_Resuelto`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `idRespuesta` FOREIGN KEY (`idRespuesta`) REFERENCES `respuesta` (`idRespuesta`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `idRespuesta` FOREIGN KEY (`idRespuesta`) REFERENCES `respuesta` (`idRespuesta`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -127,14 +127,14 @@ DROP TABLE IF EXISTS `respuesta`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `respuesta` (
-  `idRespuesta` int(11) NOT NULL,
+  `idRespuesta` int(11) NOT NULL AUTO_INCREMENT,
   `Tipo_Pregunta` int(11) NOT NULL,
   `Descripcion` varchar(100) NOT NULL,
   `idPregunta` int(11) NOT NULL,
   PRIMARY KEY (`idRespuesta`),
   KEY `idPregunta_idx` (`idPregunta`),
   CONSTRAINT `idPregunta` FOREIGN KEY (`idPregunta`) REFERENCES `pregunta` (`idPregunta`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -143,7 +143,7 @@ CREATE TABLE `respuesta` (
 
 LOCK TABLES `respuesta` WRITE;
 /*!40000 ALTER TABLE `respuesta` DISABLE KEYS */;
-INSERT INTO `respuesta` VALUES (1,1,'Día Mes Año',1),(2,2,'Abierta',2),(3,3,'1)Masculino 2)Femenino 3)Otro',3),(4,3,'1)Casado 2)Viudo(a) 3)Separada 4)Soltera(o)',4),(5,3,'1)empleado 2)negocio propio 3)jubilado 4)hogar',5),(6,3,'1)IMSS 2)ISSTE 3)Seguro popular 4) Hospital militar 5)No tiene ',6),(7,3,'1)Propia 2)Rentada 3)Prestada',7),(8,3,'1)Vive solo 2)Con su pareja 3)Algun hijo',8),(9,4,'1)Su pareja 2)Sus hijos 3)Sus hermanos',9),(10,4,'1)Problemas del corazón 2)Diabetes 3)Enfermedad pulmonar 4)Cáncer',10);
+INSERT INTO `respuesta` VALUES (1,1,'Día Mes Año',1),(2,2,'Abierta',2),(3,3,'Masculino',3),(4,3,'Casado(a)',4),(5,3,'Empleado',5),(6,3,'IMSS',6),(7,3,'Propia',7),(8,3,'Vive solo',8),(9,4,'Su pareja',9),(10,4,'Problemas del corazón',10),(11,3,'Femenino',3),(12,3,'Viudo(a)',4),(13,3,'Separado(a)',4),(14,3,'Soltero(a)',4),(15,3,'Negocio propio',5),(16,3,'Jubilado',5),(17,3,'Hogar',5),(18,3,'ISSTE',6),(19,3,'Seguro popular',6),(20,3,'ISSFAM',6),(21,3,'No tiene',6),(22,3,'Rentada',7),(23,3,'Prestada',7),(24,3,'Con pareja',8),(25,3,'Algun hijo',8),(26,3,'Sus hijos',9),(27,3,'Sus hermanos',9),(28,3,'Diabetes',10),(29,3,'Enfermedad pulmonar',10),(30,3,'Cáncer',10);
 /*!40000 ALTER TABLE `respuesta` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -160,7 +160,7 @@ CREATE TABLE `usuario` (
   `Nombre` varchar(45) NOT NULL,
   `Password` varchar(100) NOT NULL,
   PRIMARY KEY (`idUsuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -182,4 +182,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-11-26 18:43:47
+-- Dump completed on 2017-11-26 20:24:11
